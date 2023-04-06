beb9242df42642da3250f5b966639bca351bbf36
Jaeyoung Choi <jaeyoung.choi@scale.snu.ac.kr>
2023-04-06 14:36:30 +0900

    [PARAMS] Change param for 'Chiplet-HE' Project
    Applied 4x4 structure, 256B flit size.

--- src/rtl/noc/noc.sv
+++ src/rtl/noc/noc.sv
@@ -1,7 +1,8 @@
 package noc_params;
 
-	localparam MESH_SIZE_X = 5;
-	localparam MESH_SIZE_Y = 5;
+  // We will Implement a 4x4 Mesh Network
+	localparam MESH_SIZE_X = 4;
+	localparam MESH_SIZE_Y = 4;
 
 	localparam DEST_ADDR_SIZE_X = $clog2(MESH_SIZE_X);
 	localparam DEST_ADDR_SIZE_Y = $clog2(MESH_SIZE_Y);
@@ -11,11 +12,10 @@ package noc_params;
 
 	localparam HEAD_PAYLOAD_SIZE = 16;
 
-	localparam FLIT_DATA_SIZE = DEST_ADDR_SIZE_X+DEST_ADDR_SIZE_Y+HEAD_PAYLOAD_SIZE;
-
 	typedef enum logic [2:0] {LOCAL, NORTH, SOUTH, WEST, EAST} port_t;
 	localparam PORT_NUM = 5;
 	localparam PORT_SIZE = $clog2(PORT_NUM);
+  parameter PORT_WIDTH = 2048;  // 2048 bits = 256 Bytes
 
 	typedef enum logic [1:0] {HEAD, BODY, TAIL, HEADTAIL} flit_label_t;
 
@@ -26,6 +26,10 @@ package noc_params;
 		logic [HEAD_PAYLOAD_SIZE-1: 0] 	head_pl;
 	} head_data_t;
 
+  // Flit size always should be 256 Bytes
+	localparam FLIT_DATA_SIZE_VC = PORT_WIDTH - HEAD_PAYLOAD_SIZE - DEST_ADDR_SIZE_X - DEST_ADDR_SIZE_Y - VC_SIZE - 2;
+	localparam FLIT_DATA_SIZE_NOVC = PORT_WIDTH - HEAD_PAYLOAD_SIZE - DEST_ADDR_SIZE_X - DEST_ADDR_SIZE_Y - 2;
+
 	typedef struct packed
 	{
 		flit_label_t			flit_label;
@@ -33,18 +37,18 @@ package noc_params;
 		union packed
 		{
 			head_data_t 		head_data;
-			logic [FLIT_DATA_SIZE-1 : 0] bt_pl;
+			logic [FLIT_DATA_SIZE_VC-1 : 0] bt_pl;
 		} data;
 	} flit_t;
 
-    typedef struct packed
-    {
-        flit_label_t flit_label;
-        union packed
-        {
-            head_data_t head_data;
-            logic [FLIT_DATA_SIZE-1 : 0] bt_pl;
-        } data;
-    } flit_novc_t;
+  typedef struct packed
+  {
+      flit_label_t flit_label;
+      union packed
+      {
+          head_data_t head_data;
+          logic [FLIT_DATA_SIZE_NOVC-1 : 0] bt_pl;
+      } data;
+  } flit_novc_t;
 
 endpackage
