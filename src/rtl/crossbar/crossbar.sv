efc64797cd7bf61e0bd28048e407a17da7357d45
Jaeyoung Choi <jaeyoung.choi@scale.snu.ac.kr>
2023-04-11 17:25:35 +0900

    Add buffers
    Add pipelined buffer register in crossbar for output and input_port for input

--- src/rtl/crossbar/crossbar.sv
+++ src/rtl/crossbar/crossbar.sv
@@ -1,3 +1,4 @@
+`timescale 1ns / 1ps
 import noc_params::*;
 
 module crossbar #(
@@ -7,17 +8,31 @@ module crossbar #(
     output flit_t data_o [PORT_NUM-1:0]
 );
 
+    // output register buffer to crossbar
+    reg [$bits(flit_t)-1:0] data_o_reg [PORT_NUM-1:0];
+    
     /*
     Combinational logic:
     on each output, propagate the corresponding input
     according to the current selection
+    Added buffer to meet timing
     */
     always_comb
     begin
         for(int ip = 0; ip < PORT_NUM; ip = ip + 1)
         begin
-            data_o[ip] = ib_if.flit[sa_if.input_vc_sel[ip]];
+            data_o[ip] <= data_o_reg[ip];
         end
+
+        for(int ip = 0; ip < PORT_NUM; ip = ip + 1)
+        begin
+            data_o_reg[ip] <= ib_if.flit[sa_if.input_vc_sel[ip]];
+        end
+        
+        // for(int ip = 0; ip < PORT_NUM; ip = ip + 1)
+        // begin
+        //     data_o[ip] <= ib_if.flit[sa_if.input_vc_sel[ip]];
+        // end
     end
 
 endmodule
--- src/rtl/noc/noc.sv
+++ src/rtl/noc/noc.sv
@@ -1,53 +1,54 @@
+`timescale 1ns / 1ps
 package noc_params;
 
-  // We will Implement a 4x4 Mesh Network
-	localparam MESH_SIZE_X = 4;
-	localparam MESH_SIZE_Y = 4;
+  // We will implement a 4x4 Mesh Network
+	parameter MESH_SIZE_X = 4;
+	parameter MESH_SIZE_Y = 4;
 
 	localparam DEST_ADDR_SIZE_X = $clog2(MESH_SIZE_X);
 	localparam DEST_ADDR_SIZE_Y = $clog2(MESH_SIZE_Y);
 
-	localparam VC_NUM = 2;
+	parameter VC_NUM = 4;
 	localparam VC_SIZE = $clog2(VC_NUM);
 
 	localparam HEAD_PAYLOAD_SIZE = 16;
 
 	typedef enum logic [2:0] {LOCAL, NORTH, SOUTH, WEST, EAST} port_t;
-	localparam PORT_NUM = 5;
+	parameter PORT_NUM = 5;
 	localparam PORT_SIZE = $clog2(PORT_NUM);
-  parameter PORT_WIDTH = 2048;  // 2048 bits = 256 Bytes
+  localparam PORT_WIDTH = 512;  // 512 bits = 64 Bytes
+  parameter BUFFER_SIZE = 8;   // 8 flits
 
 	typedef enum logic [1:0] {HEAD, BODY, TAIL, HEADTAIL} flit_label_t;
 
 	typedef struct packed
 	{
-		logic [DEST_ADDR_SIZE_X-1 : 0] 	x_dest;
-		logic [DEST_ADDR_SIZE_Y-1 : 0] 	y_dest;
-		logic [HEAD_PAYLOAD_SIZE-1: 0] 	head_pl;
+		logic [DEST_ADDR_SIZE_X-1 : 0] 	x_dest; //2
+		logic [DEST_ADDR_SIZE_Y-1 : 0] 	y_dest; //2
+		logic [HEAD_PAYLOAD_SIZE-1: 0] 	head_pl;  //16
 	} head_data_t;
 
   // Flit size always should be 256 Bytes
-	localparam FLIT_DATA_SIZE_VC = PORT_WIDTH - HEAD_PAYLOAD_SIZE - DEST_ADDR_SIZE_X - DEST_ADDR_SIZE_Y - VC_SIZE - 2;
-	localparam FLIT_DATA_SIZE_NOVC = PORT_WIDTH - HEAD_PAYLOAD_SIZE - DEST_ADDR_SIZE_X - DEST_ADDR_SIZE_Y - 2;
+	localparam FLIT_DATA_SIZE = PORT_WIDTH - HEAD_PAYLOAD_SIZE - DEST_ADDR_SIZE_X - DEST_ADDR_SIZE_Y - VC_SIZE - 2;
 
 	typedef struct packed
 	{
 		flit_label_t			flit_label;
 		logic [VC_SIZE-1 : 0] 	vc_id;
-		union packed
+		struct packed
 		{
 			head_data_t 		head_data;
-			logic [FLIT_DATA_SIZE_VC-1 : 0] bt_pl;
+			logic [FLIT_DATA_SIZE-1 : 0] bt_pl;
 		} data;
 	} flit_t;
 
   typedef struct packed
   {
       flit_label_t flit_label;
-      union packed
+      struct packed
       {
           head_data_t head_data;
-          logic [FLIT_DATA_SIZE_NOVC-1 : 0] bt_pl;
+          logic [FLIT_DATA_SIZE-1 : 0] bt_pl;
       } data;
   } flit_novc_t;
 
